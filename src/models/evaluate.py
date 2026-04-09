bbf131c37ee453356a40b5faee3569388ecc24e7
kagaaa1 <mohdkaif3003@gmail.com>
2026-04-09 23:15:48 +0530

    Update evaluation script to load data and split dataset
    Refactor evaluation process to include data loading and train-test split.

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import joblib
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+
 model = joblib.load('models/spam_model.pkl')
+
+df = pd.read_csv('data/cleaned/spam_clean.csv')
+
+X = df['text']
+y = df['label']
+
+_, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 predictions = model.predict(X_test)
 
-score = accuracy_score(y_test, predictions)
-
-print(f"The robot got a gradeof: {score * 100}%")
-print("\nHere is the full report :")
+print(f"Accuracy Score: {accuracy_score(y_test, predictions) * 100:.2f}%")
+print("\nDetailed Performance Report:")
 print(classification_report(y_test, predictions))
