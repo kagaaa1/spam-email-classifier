6ec929b3c732ccdc800837582dba1faec5a94b8b
kagaaa1 <mohdkaif3003@gmail.com>
2026-04-09 23:28:14 +0530

    Refactor evaluate.py to include vectorization
    Updated evaluation script to use vectorized input and improved output messages.

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -3,17 +3,20 @@ import joblib
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 
+
 model = joblib.load('models/spam_model.pkl')
+vectorizer = joblib.load('models/vectorizer.pkl')
 
 df = pd.read_csv('data/cleaned/spam_clean.csv')
-
 X = df['text']
 y = df['label']
 
-_, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_numeric = vectorizer.transform(X)
+
+_, X_test, _, y_test = train_test_split(X_numeric, y, test_size=0.2, random_state=42)
 
 predictions = model.predict(X_test)
 
-print(f"Accuracy Score: {accuracy_score(y_test, predictions) * 100:.2f}%")
-print("\nDetailed Performance Report:")
+print(f"Accuracy: {accuracy_score(y_test, predictions) * 100:.2f}%")
+print("\nFull Report:")
 print(classification_report(y_test, predictions))
